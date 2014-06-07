f9b924e37a134a60189afc6ea77b95a6c0f7c5fb
Aman Madaan <madaan.amanmadaan@gmail.com>
2014-06-07 18:49:34 +0530

    Added patch and fixed indentation

--- hello.cpp
+++ hello.cpp
@@ -10,14 +10,11 @@ int div(int a, int b) {
 	if (b != 0){
     	return a / b;
 	}
-	
 	else return INF;
-	
 }
 
-int mul (int a,int b)
-{
-    return a*b;
+int mul (int a, int b) {
+    return a * b;
 }
 
 int main() {
